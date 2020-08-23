e3dcfb1c14be4fca14f1ad19db84624da34e25ae
NDK1410 <khanh.nd1410@gmail.com>
2020-08-23 19:27:27 +0700

    header-listSongs-playSong

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <router-link :to="{ name: 'HelloWorld' }">Home</router-link>
-    <router-link to="/playlist">Playlist</router-link>
+    <header>
+      <router-link :to="{ name: 'HelloWorld' }">Home</router-link>
+      <router-link to="/playlist">Playlist</router-link>
+    </header>
     <router-view/>
   </div>
 </template>
@@ -13,12 +15,32 @@ export default {
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+header {
+  padding-top: 30px;
+  padding-bottom: 30px;
+  background-color: black;
+  color: white;
+  font-weight: bold;
+  font-size: 40px;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
 </style>
